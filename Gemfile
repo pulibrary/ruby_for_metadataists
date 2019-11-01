1e4ec1f8d0e061dbdc10c56e8da0524e306b63c4
Mark Zelesky <mzelesky@princeton.edu>
2019-11-01 09:39:15 -0400

    merging holdings into bibs

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 source "https://rubygems.org"
 
-gem "nokogiri"
+gem "irb", require: false
+gem "nokogiri", ">= 1.10.4"
 gem "marc"
+gem "ruby-oci8"
---
+++ marc/get_sc_bibs_with_items_xml.rb
@@ -0,0 +1,92 @@
+require_relative './dump_methods'
+require_relative './format_methods'
+
+### Create array of all locations of interest; taken from Don's spreadsheet
+sc_locations = %w[
+  beac
+  cook
+  ctsn
+  ctsnrf
+  ed
+  ex
+  exb
+  exc
+  exca
+  exf
+  exho
+  exi
+  exka
+  exki
+  exl
+  exme
+  exov
+  expa
+  exrc
+  exrl
+  extr
+  extsf
+  exv
+  exw
+  ga
+  garf
+  gax
+  hsvc
+  hsve
+  hsvg
+  hsvm
+  hsvn
+  hsvp
+  hsvr
+  hsvw
+  htn
+  map
+  mss
+  mudd
+  mudf
+  njpg
+  num
+  numrf
+  pb
+  prnc
+  ptt
+  rcppg
+  rcpph
+  rcpxc
+  rcpxg
+  rcpxm
+  rcpxn
+  rcpxp
+  rcpxr
+  rcpxw
+  rht
+  thx
+  thxr
+  vrg
+  w
+  wa
+  warf
+  whs
+  wit
+]
+
+### Establish ODBC connection to Voyager
+conn_file = File.size?('./../tmp/conn.yml') ? './../tmp/conn.yml' : './conn.yml'
+stanza = YAML.load_file(conn_file)
+conn = OCI8.new(stanza['user'], stanza['pass'], stanza['name'])
+
+### Get bib IDs of interest
+bib_ids = get_bib_ids_for_locations(sc_locations, conn)
+
+### Export merged bibs, allowing for oversized records
+merged_writer = MARC::XMLWriter.new('./all_sc_bibs_with_holdings_items.xml')
+
+### Also export unmerged bibs
+unmerged_writer = MARC::XMLWriter.new('./all_sc_bibs.xml')
+bib_ids.each_slice(1000) do |segment|
+  merged_bibs = get_merged_bibs(segment, conn)
+  merged_bibs.each { |bib| merged_writer.write(bib) }
+  unmerged_bibs = get_bib_coll(segment, conn)
+  unmerged_bibs.each { |bib| unmerged_writer.write(bib) }
+end
+merged_writer.close
+unmerged_writer.close
